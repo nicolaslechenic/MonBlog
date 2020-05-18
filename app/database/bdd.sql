5b0f3047807adddf6e54a63279b457005c7dffdf
Stéphanie lemaitre <stephanie.lemaitre56@gmail.com>
2020-05-18 18:52:19 +0200

    bdd

--- app/database/bdd.sql
+++ app/database/bdd.sql
@@ -11,13 +11,39 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Listage de la structure de la base pour monblog
+CREATE DATABASE IF NOT EXISTS `monblog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `monblog`;
+
+-- Listage de la structure de la table monblog. admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
 -- Listage des données de la table monblog.admin : ~1 rows (environ)
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` (`id`, `pseudo`, `password`, `email`) VALUES
-	(5, 'admin', '$2y$10$U9rwCRnPlLiSM30Ztq.xTOqcu8xjn.YIt5EZGHgvqqi6042HuNAX2', 'dec-steph@gmail.com');
+	(5, 'admin', '$2y$10$U9rwCRnPlLiSM30Ztq.xTOqcu8xjn.YIt5EZGHgvqqi6042HuNAX2', 'dev-steph@gmail.com');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 
--- Listage des données de la table monblog.article : ~7 rows (environ)
+-- Listage de la structure de la table monblog. article
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `ref_page` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(1000) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `creation_date` date NOT NULL,
+  `update_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+-- Listage des données de la table monblog.article : ~8 rows (environ)
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
 INSERT INTO `article` (`id`, `ref_page`, `title`, `image`, `content`, `creation_date`, `update_date`) VALUES
 	(9, 'Australie', 'Le départ et l\'arrivée', 'australie1.jpg', 'On a beau savoir où on va, comment on n\'y va et combien de temps ça prendra, on n\'est jamais vraiment préparé !\r\nD\'un on a TOUS tendance à prendre trop ! Le \'au cas ou\' qui prend 14kilos dans votre valise ! Genre, c\'est bien connu, des pharmacies y en a qu\'en France !\r\nOn se prend le gros gel douche, le gros shampoo pour ne pas manquer alors qu\'il est tellement plus logique d\'en racheter direct là-bas car oui y a aussi des super-marchés ! Le truc de dingue !\r\n\r\nPour aller en Australie, à Perth, si on veut être précis, on a un peu de route !\r\nDéjà 3h30 de train pour aller à Paris, puis 2h d\'avion pour aller à Londres et après 4h d\'escale à baver devant un cookie à 4euros et on décolle enfin pour ...17h de vol. Le vol de plus long et sans escale.\r\n\r\nOui, c\'est très long et à la limite du supportable surtout quand on n\'arrive pas à dormir assis. Impossible de s\'allonger ni de tendre ses jambes.... Pas conseillé d\'être en surpoids sauf si vous avez les moyens de vous payer une classe supérieure !\r\nOn a beau avoir pris un livre, une console de jeux portable, son MP4, on n\'a envie de rien d\'autre que de courir et sauter pour se dégourdir les jambes et la tête !\r\nAlors quand on voit les côtes Australiennes.... ahhh que du bonheur !\r\n\r\nOn sort enfin de l\'aéroport, il est 11h à Perth, et je n\'ai pas dormi depuis plus de 22h ! ET pourtant pas fatiguée, tellement heureuse d\'être là !', '2020-04-07', '2020-04-07'),
@@ -26,13 +52,25 @@ INSERT INTO `article` (`id`, `ref_page`, `title`, `image`, `content`, `creation_
 	(12, 'Australie', 'Les randonnées', 'australie6.JPG', 'Une bonne paire de chaussure et une bonne paire de chaussette ( les fournis ne sont pas très sympathique !) et hop, c\'est parti pour une belle balade ! \r\n\r\nEloignez-vous un peu des sentiers hyper touristes et là... oui là c\'est que du bonheur ! Vous êtes seul ou presque !\r\n\r\nmais comment savoir où aller ?\r\n\r\nSi vous êtes passé par une agence de voyages ( une qui a des conseillés vivant sur place c\'est encore mieux ! ), demandez lui de vous faire un petit \'routard\' des choses à voir et à faire.\r\n\r\nAllez sur des forums et demandez directement à des australiens qui vous parlerons des lieux à voir !\r\n\r\nOu sinon, des Offices de tourisme ! C\'est comme ça qu\'on s\'est offert un virée pour voir des baleines !\r\n\r\nInvestissez dans un bon appareil photo ( sans aller jusqu\'à 1000 euros hein !). Quand je vois des gens qui prennent des photos avec les tablettes.... T.T\r\n\r\n', '2020-04-07', '2020-04-07'),
 	(13, 'Nouvelle-zelande', 'Moitié du voyage', 'n.z1.jpg', 'Quand on a pris l\'avion, un petit coucou, pour aller direction la Nouvelle-Zélande, on a versé notre petit larme.\r\nQuitter l\'Australie avec la sensation de ne rien avoir vu était un crève cœur et pourtant on n\'a pas eu un instant de farniente ! \r\nJ\'ai adoré l\'Australie mais en arrivant en Nouvelle-Zélande, je suis tombée à genoux ! \r\nAtterrir entre deux chaînes de montagne enneigées... magique.\r\nArrivée avec 22 degrés et un soleil de dingue ! J\'ai retrouvé le sourire !\r\n\r\nPetit soucis au moment de récupérer notre voiture de location. Notre voiture est parti avec un autre client...ah bon ? bah surclassé alors ! ;D\r\n\r\nToute l\'île du sud de la Nouvelle-Zélande est quasi occupée que des prairies pour les moutons ! Y\'en a partout sur une surface verdoyante impressionnante !\r\n\r\nOn ne s\'est jamais lassé un seul instant du paysage ! Et nous avons eu plein de surprises ! Surtout avec la météo !\r\n\r\n\r\n\r\n', '2020-04-07', '2020-04-07'),
 	(14, 'Nouvelle-zelande', 'Aéroport', 'n.z04.jpg', 'Fan de seigneurs des anneaux ?\r\n\r\nAlors la Nouvelle-Zélande est faite pour vous !\r\n\r\nDans certains aéroport, vous serez accueillis par des aigles géants chevauchés par Gandalf ou bien la tête du dragon Smaug.\r\n\r\nSi vous êtes vraiment intéressé, pensez a bien vous renseigner et vous équiper car grimper la montagne du destin est un vrai challenge !\r\n\r\nÉvitez les trucs trop touristique, encore une fois, vous allez être aussi déçu que nous. Le film a déjà quelques années et certains lieux sont vieux et ont perdu tout intérêt et vous allez payer cher !\r\n\r\nMon barbu avait des petites étoiles dans les yeux...en sortant du studio Weta où a été tourné les films, c\'était des larmes ! \r\n5 pièces remplis d\'objets de décor, certains n\'ont aucun rapport avec les films, style la voiture le Halo... Ou encore des épées qui n\'ont jamais été utilisées dans les films. A oui et aucune visite ne français donc si vous n’êtes pas à l\'aise avec l\'anglais vous ne comprendrez rien ! Ils parlent super vite !\r\n\r\nLe magasin : petit et hyper cher et la moitié des articles en rupture de stock !\r\n\r\nVous voulez vraiment voir du seigneurs des anneaux ? Faites Hobbits-town !', '2020-04-07', '2020-04-07'),
-	(16, 'Trucs_et_astuces', 'Permis', 'trucs02.jpg', 'Deux trois choses à savoir !\r\n1/ Ils conduisent à gauche.\r\n2/ Leurs signalétiques différent du notre ( pas tout heureusement !)\r\n3/ Quasi toutes les voitures que vous louerez sont automatiques !\r\n4/ c\'est pas du d\'avoir un permis provisoire !\r\n\r\nRenseignez-vous, regardez une ou deux vidéos sur le code de conduite et pourquoi ne pas aller chez un concessionnaire pour tester une voiture auto pour se rassurer ?', '2020-04-07', '2020-04-07');
+	(16, 'Trucs_et_astuces', 'Permis', 'trucs02.jpg', 'Deux trois choses à savoir !\r\n1/ Ils conduisent à gauche.\r\n2/ Leurs signalétiques différent du notre ( pas tout heureusement !)\r\n3/ Quasi toutes les voitures que vous louerez sont automatiques !\r\n4/ c\'est pas du d\'avoir un permis provisoire !\r\n\r\nRenseignez-vous, regardez une ou deux vidéos sur le code de conduite et pourquoi ne pas aller chez un concessionnaire pour tester une voiture auto pour se rassurer ?', '2020-04-07', '2020-04-07'),
+	(19, 'Australie', 'qsdfghj', 'IMG_5555.JPG', 'xcvbn', '2020-05-18', '2020-05-18');
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 
--- Listage des données de la table monblog.commentaire : ~1 rows (environ)
+-- Listage de la structure de la table monblog. commentaire
+CREATE TABLE IF NOT EXISTS `commentaire` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_pseudo` varchar(255) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` date NOT NULL,
+  `update_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_commentaire_article` (`article_id`),
+  CONSTRAINT `FK_commentaire_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Listage des données de la table monblog.commentaire : ~0 rows (environ)
 /*!40000 ALTER TABLE `commentaire` DISABLE KEYS */;
-INSERT INTO `commentaire` (`id`, `user_pseudo`, `ref_page`, `content`, `creation_date`, `update_date`) VALUES
-	(1, 'fghjkl', 'australie', 'fghjklmm:!', '2020-05-13', '2020-05-13');
 /*!40000 ALTER TABLE `commentaire` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
